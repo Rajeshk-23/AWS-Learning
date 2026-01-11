9882f5fd317cc2c4d8118ab932bc03f9665fa66c
KURNUTHALA RAJESH KUMAR <krazesh7@gmail.com>
2026-01-11 14:28:02 +0530

    Update app.py

--- Day15-AWS CICD/simple-python/app.py
+++ Day15-AWS CICD/simple-python/app.py
@@ -1,3 +1,4 @@
+#app.py
 from flask import Flask
 
 app = Flask(__name__)
@@ -7,4 +8,5 @@ def hello():
     return 'Hello, Raaju world!'
 
 if __name__ == '__main__':
+
     app.run(host='0.0.0.0', port=5000)
